73336463ac9e78709ed2653d090eed4404c6c301
Ruhan Khandakar <ruhankhandaker@igmail.com>
2019-02-25 22:07:32 +0530

    Add Router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 import foodReducer from "./reducers";
@@ -17,7 +18,9 @@ const store = createStore(
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </Provider>,
     document.getElementById("root")
 );
