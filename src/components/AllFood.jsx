b64d4497d3bc7b9875a92915c7762412ad3045eb
Ruhan Khandakar <ruhankhandaker@igmail.com>
2019-02-25 22:21:29 +0530

    Add details to all food component

--- src/components/AllFood.jsx
+++ src/components/AllFood.jsx
@@ -1,33 +1,34 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Row, Col, Table } from "react-bootstrap";
 
-const AllFood = () => {
+const AllFood = ({ foods }) => {
     return (
         <React.Fragment>
             <Row>
                 <Col />
-                <Col md={6}>
-                    <div className="shadow p-3 mb-5 m-auto bg-white rounded">
+                <Col md={6} className="mb-5">
+                    <div className="shadow p-3  m-auto bg-white rounded">
                         <h3 className="text-center">All Food Details</h3>
+                        <span className="float-right text-muted">
+                            <sup>*</sup>100gm base value
+                        </span>
                         <Table bordered hover>
                             <thead>
                                 <tr>
-                                    <th>First Name</th>
-                                    <th>Last Name</th>
-                                    <th>Username</th>
+                                    <th>Name</th>
+                                    <th>Calories</th>
+                                    <th>Fat</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                </tr>
-                                <tr>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                </tr>
+                                {foods.map(food => (
+                                    <tr key={food.id}>
+                                        <td>{food.title}</td>
+                                        <td>{food.calroie}</td>
+                                        <td>{food.fat} g</td>
+                                    </tr>
+                                ))}
                             </tbody>
                         </Table>
                     </div>
@@ -38,4 +39,9 @@ const AllFood = () => {
     );
 };
 
-export default AllFood;
+// Get ALl foods
+const mapStateToProps = state => ({
+    foods: state.foods
+});
+
+export default connect(mapStateToProps)(AllFood);
