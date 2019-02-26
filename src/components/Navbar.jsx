9f0fc70a19b0933d9b572cb6de10b4da466f7efb
Ruhan Khandakar <ruhankhandaker@igmail.com>
2019-02-26 11:27:41 +0530

    Add Link Router

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Navbar } from "react-bootstrap";
 const NavBar = () => {
     return (
         <React.Fragment>
-            <Navbar className="mb-3 p-0 nav-bg">
-                <p className="display-4 text-white py-1 pb-2 m-auto">
+            <Navbar className="mb-3 p-0 nav-bg justify-content-around flex-column flex-md-row ">
+                <p className="">
+                    <Link to="/">
+                        <i className="fas fa-home fa-2x" />
+                    </Link>
+                </p>
+                <p className="display-4 text-white py-1 pb-2">
                     Be Healthy Be Happy
+                </p>
+                <p className="details">
+                    <Link to="/details">
+                        All Foods <i className="fas fa-utensils fa-2x" />
+                    </Link>
                 </p>
             </Navbar>
         </React.Fragment>
