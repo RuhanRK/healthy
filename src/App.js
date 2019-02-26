47fa9385affd25bd24755b5d1697788323874dc2
Ruhan Khandakar <ruhankhandaker@igmail.com>
2019-02-26 17:14:18 +0530

    Change Router from index to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect
+} from "react-router-dom";
 
 import NavBar from "./components/Navbar";
 import MainContainer from "./components/MainContainer";
@@ -9,15 +14,17 @@ import NotFoundPage from "./components/NotFoundPage";
 class App extends Component {
     render() {
         return (
-            <React.Fragment>
-                <NavBar />
-                <Switch>
-                    <Route path="/" exact component={MainContainer} />
-                    <Route path="/details" component={AllFood} />
-                    <Route path="/not-found" component={NotFoundPage} />
-                    <Redirect to="/not-found" />
-                </Switch>
-            </React.Fragment>
+            <Router>
+                <React.Fragment>
+                    <NavBar />
+                    <Switch>
+                        <Route path="/" exact component={MainContainer} />
+                        <Route path="/details" component={AllFood} />
+                        <Route path="/not-found" component={NotFoundPage} />
+                        <Redirect to="/not-found" />
+                    </Switch>
+                </React.Fragment>
+            </Router>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 import foodReducer from "./reducers";
@@ -18,9 +17,7 @@ const store = createStore(
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router>
-            <App />
-        </Router>
+        <App />
     </Provider>,
     document.getElementById("root")
 );
