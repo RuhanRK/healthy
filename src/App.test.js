2b309a894a6da963a5107ea06e5e7d2b7f76720e
Ruhan Khandakar <ruhankhandaker@igmail.com>
2019-02-26 18:56:20 +0530

    Add Some test with enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,19 @@
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
+
 import * as types from "./actions/actions";
 import * as actions from "./actions/";
+import NavBar from "./components/Navbar";
+import MainContainer from "./components/MainContainer";
+
+let wrapper = shallow(<App />);
+
+describe("See components inside of App", () => {
+    it("shows a NavBar component", () => {
+        expect(wrapper.find(NavBar).length).toEqual(1);
+    });
+});
 
 describe("actions for search box", () => {
     it("should found a food", () => {
