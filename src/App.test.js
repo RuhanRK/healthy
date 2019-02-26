e6ebea2e5d927867280a14312e5ff252b4c1be25
Ruhan Khandakar <ruhankhandaker@igmail.com>
2019-02-26 17:21:32 +0530

    Write a test for search food

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import * as types from "./actions/actions";
+import * as actions from "./actions/";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("actions for search box", () => {
+    it("should found a food", () => {
+        const text = "coke";
+        const expectedAction = {
+            type: types.FOOD_SELECT,
+            payload: text
+        };
+        expect(actions.selectFood(text)).toEqual(expectedAction);
+    });
 });
